2870a63ad2861b5f2753c1d0e9d545e6d2ddfb88
augustoalves.sep@gmail.com <guto@uget.com.br>
2024-06-21 22:13:28 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { toast, ToastContainer } from 'react-toastify'
+
+function App() {
+  const [posicaoY, setPosicaoY] = useState()
+  const [posicaoX, setPosicaoX] = useState()
+
+  const [vezes, setVezes] = useState(0)
+ 
+  function alterarPosicão(){
+    setPosicaoX(Math.random() * 90)
+    setPosicaoY(Math.random() * 90)
+
+    setVezes(Number(vezes) + 1)
+  }
+
+
+  useEffect(()=>{
+    if(vezes > 2 && vezes < 5){
+      toast.error('Essa opção não vale, manda ai que eu mereço')
+    } else if (vezes > 10) {
+      toast.error('É serio q ainda está tentando, manda ai vái')
+    }
+    
+  },[posicaoX])
+
+
+  return (
+    <main className='container'>
+
+      <h1>FOTO DO PEITO HOJE?</h1>
+
+      <ToastContainer/>
+
+      <div className='buttons'>
+
+        <a target='_blank' href='https://www.youtube.com/watch?v=tDpy-KnySNw'>
+          <button className='yes' onClick={() => toast('obrigado amor, estou esperando no meu whatsapp ratazana ❤️❤️❤️')}>SIM</button>
+        </a>
+        
+         <button className='no' style={vezes > 0 ? {position: 'absolute', left: `${posicaoX}%`, top:`${posicaoY}%`} : {}} onMouseEnter={alterarPosicão}>NÃO</button>
+      </div>
+
+    </main>
+  );
+}
+
+export default App;
